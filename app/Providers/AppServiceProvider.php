1ff01b2b6e67d36d0ffa55e3b0655aeb31a2996a
Matia Mathias <80642278+nm1999@users.noreply.github.com>
2026-05-02 23:51:31 +0300

    Updated AppServiceProvider to create storage symlink if it doesn't exist

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,30 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Vite::prefetch(concurrency: 3);
+        
+        // Automatically create storage symlink if it doesn't exist
+        $this->createStorageSymlink();
+    }
+
+    /**
+     * Create storage symlink if it does not exist.
+     *
+     * @return void
+     */
+    protected function createStorageSymlink(): void
+    {
+        $publicStoragePath = public_path('storage');
+        $storageAppPublicPath = storage_path('app/public');
+
+        // Check if symlink doesn't exist
+        if (!file_exists($publicStoragePath) && !is_link($publicStoragePath)) {
+            try {
+                // Try to create symlink
+                Artisan::call('storage:link');
+            } catch (\Exception $e) {
+                // Silently fail - symlink creation might not be supported on all servers
+                // The /storage-link route can be used as fallback
+            }
+        }
     }
 }

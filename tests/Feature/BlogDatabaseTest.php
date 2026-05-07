52bd7fcfb4b615a75f900c812767ee5c009a5c90
matia mathias <nyanzimathiaz@gmail.com>
2026-05-07 20:57:38 +0300

    more changes in test

--- tests/Feature/BlogDatabaseTest.php
+++ tests/Feature/BlogDatabaseTest.php
@@ -10,7 +10,7 @@ class BlogDatabaseTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_blog_page_reads_sermons_from_database(): void
+    public function test_activities_page_reads_sermons_from_database(): void
     {
         $this->withoutVite();
 
